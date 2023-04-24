27eb139dc6208e166fc0ae6787e50972028a9a3a
komediantto <komediantto@yandex.ru>
2023-04-24 16:41:24 +0200

    Initial commit

--- venv/lib/python3.10/site-packages/fastapi_admin/models.py
+++
@@ -1,9 +0,0 @@
-from tortoise import Model, fields
-
-
-class AbstractAdmin(Model):
-    username = fields.CharField(max_length=50, unique=True)
-    password = fields.CharField(max_length=200)
-
-    class Meta:
-        abstract = True
--- venv/lib/python3.10/site-packages/fastapi_admin/routes/__init__.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, Depends
-
-from fastapi_admin.depends import get_current_admin
-
-from .resources import router as resources_router
-
-router = APIRouter()
-router.include_router(resources_router, dependencies=[Depends(get_current_admin)])
